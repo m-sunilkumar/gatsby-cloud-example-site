6a51673afa7095eaab4d5629451954c8c52c666a
sunil_kumar_060 <sunil.m@mindtree.com>
2021-04-05 18:16:31 +0530

    configured contentfull

--- gatsby-theme-event/gatsby-config.js
+++ gatsby-theme-event/gatsby-config.js
@@ -1,4 +1,12 @@
 const path = require("path");
+
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
+console.log(`Using environment config: '${activeEnv}'`);
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+});
 module.exports = {
   plugins: [
     {
@@ -10,10 +18,11 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: "",
-        accessToken: "",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
